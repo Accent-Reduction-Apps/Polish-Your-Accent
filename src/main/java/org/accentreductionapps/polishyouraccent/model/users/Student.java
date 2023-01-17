2e52815780a6c7996ce22ea567b746563b4f3b31
Dawid Deszcz <d.deszcz@gmail.com>
2023-01-17 10:49:50 +0100

    Update POM

--- src/main/java/org/accentreductionapps/polishyouraccent/model/users/Student.java
+++ src/main/java/org/accentreductionapps/polishyouraccent/model/users/Student.java
@@ -1,13 +1,17 @@
 package org.accentreductionapps.polishyouraccent.model.users;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import org.accentreductionapps.polishyouraccent.model.exercises.Exercises;
 import org.accentreductionapps.polishyouraccent.model.Lesson;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 import java.util.List;
 
-@Entity
+
 public class Student extends User {
+
     private int age;
     private String gender;
     private String howLongHaveYouBeenLearningEnglish;
@@ -17,10 +21,13 @@ public class Student extends User {
 
     private List<Lesson> listOfLessonsDone;
     private List<Exercises> listOfTestsDone;
+    private Long id;
+
 
     public Student(String emailAddress, String password) {
         super(emailAddress, password);
     }
+
 
 
 }
--- src/main/java/org/accentreductionapps/polishyouraccent/model/users/User.java
+++ src/main/java/org/accentreductionapps/polishyouraccent/model/users/User.java
@@ -1,12 +1,19 @@
 package org.accentreductionapps.polishyouraccent.model.users;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+
 public abstract class User {
 
     private final String emailAddress;
     private final String password;
+    private Long id;
 
     public User(String emailAddress, String password) {
         this.emailAddress = emailAddress;
         this.password = password;
     }
+
+
 }
