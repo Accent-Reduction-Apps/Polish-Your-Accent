681da8a579415834070cc6f3cc6ee17a5d8f0a6e
Dawid Deszcz <d.deszcz@gmail.com>
2023-01-18 02:09:00 +0100

    Update UserController.java

--- src/main/java/org/accentreductionapps/polishyouraccent/controller/UserController.java
+++ src/main/java/org/accentreductionapps/polishyouraccent/controller/UserController.java
@@ -1,10 +1,9 @@
 package org.accentreductionapps.polishyouraccent.controller;
 
 
-import org.accentreductionapps.polishyouraccent.repository.UserRepository;
+
 import org.accentreductionapps.polishyouraccent.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,12 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/new-account")
 public class UserController {
 
-    private UserRepository userRepository;
     private UserService userService;
 
     @Autowired
-    public UserController(UserRepository userRepository, UserService userService) {
-        this.userRepository = userRepository;
+    public UserController(UserService userService) {
         this.userService = userService;
     }
 
