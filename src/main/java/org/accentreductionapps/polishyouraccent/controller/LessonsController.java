9bda49f36d14faa3397b05b4445c884b63489fa6
Dawid Deszcz <d.deszcz@gmail.com>
2023-01-18 19:07:17 +0100

    Refactor LessonsController
    Remove empty space

--- src/main/java/org/accentreductionapps/polishyouraccent/controller/LessonsController.java
+++ src/main/java/org/accentreductionapps/polishyouraccent/controller/LessonsController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-public class LessonsController {
+public class    LessonsController {
 
     final
     LessonsService lessonsService;
