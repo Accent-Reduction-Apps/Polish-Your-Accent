afd6360539acced83885cf912d3ecc06273c3c89
MHN468 <sebastian.sosin@motorolasolutions.com>
2023-01-19 23:44:27 +0100

    Add methods for creating new Users Student/Specialist.
    Signed-off-by: MHN468 <sebastian.sosin@motorolasolutions.com>

--- src/main/java/org/accentreductionapps/polishyouraccent/service/UserService.java
+++ src/main/java/org/accentreductionapps/polishyouraccent/service/UserService.java
@@ -1,5 +1,7 @@
 package org.accentreductionapps.polishyouraccent.service;
 
+import org.accentreductionapps.polishyouraccent.model.users.Specialist;
+import org.accentreductionapps.polishyouraccent.model.users.Student;
 import org.accentreductionapps.polishyouraccent.model.users.User;
 import org.accentreductionapps.polishyouraccent.repository.UserRepository;
 import org.springframework.stereotype.Service;
@@ -13,5 +15,20 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
-    public User getUser(Long id){return userRepository.getModelById(id);}
+    public User createNewStudent(String emailAddress, String password, int age, String gender,
+                                 String howLongHaveYouBeenLearningEnglish) {
+        User newStudent = new Student(emailAddress, password, age, gender, howLongHaveYouBeenLearningEnglish, 0, "default", null);
+        userRepository.addModel(newStudent);
+        return newStudent;
+    }
+
+    public User createNewSpecialist(String emailAddress, String password, Integer verificationCode) {
+        User newSpecialist = new Specialist(emailAddress, password, verificationCode);
+        userRepository.addModel(newSpecialist);
+        return newSpecialist;
+    }
+
+    public User getUser(Long id) {
+        return userRepository.getModelById(id);
+    }
 }
