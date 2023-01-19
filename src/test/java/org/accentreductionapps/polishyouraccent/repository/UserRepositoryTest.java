9d7d223680e3920ed8701935090732dcf0f3bc26
Dawid Deszcz <d.deszcz@gmail.com>
2023-01-19 14:35:57 +0100

    Remove bug from UserRepositoryTest.java
    accidentally removed line was added again

--- src/test/java/org/accentreductionapps/polishyouraccent/repository/UserRepositoryTest.java
+++ src/test/java/org/accentreductionapps/polishyouraccent/repository/UserRepositoryTest.java
@@ -28,6 +28,7 @@ class UserRepositoryTest {
         assertEquals(0, sizeBeforeModelAdded);
 
 //        Long idReturned = userRepository.addModel(user); //if addModel returned id, might be useful but too many changes in others code to implement now
+        userRepository.addModel(user);
 
         int sizeAfterModelAdded = userRepository.getAllModels().size();
         assertEquals(1, sizeAfterModelAdded);
