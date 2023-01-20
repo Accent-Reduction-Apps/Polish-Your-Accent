43f63f934f109cfcbfd10ceaeca8f4b919b5cb10
MHN468 <sebastian.sosin@motorolasolutions.com>
2023-01-20 05:08:21 +0100

    Create Tests class.
    Signed-off-by: MHN468 <sebastian.sosin@motorolasolutions.com>

---
+++ src/test/java/org/accentreductionapps/polishyouraccent/controller/LoginControllerTest.java
@@ -0,0 +1,19 @@
+package org.accentreductionapps.polishyouraccent.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class LoginControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void displayLoginPage() {
+    }
+
+    @Test
+    void sendLoginCredentials() {
+    }
+}
