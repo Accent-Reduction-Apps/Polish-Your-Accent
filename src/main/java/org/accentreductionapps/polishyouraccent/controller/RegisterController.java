7c15b6c7dd4fe9444f8a530efece956d641c5bfe
MHN468 <sebastian.sosin@motorolasolutions.com>
2023-01-20 02:19:01 +0100

    Add POST creating new Users.
    Signed-off-by: MHN468 <sebastian.sosin@motorolasolutions.com>

--- src/main/java/org/accentreductionapps/polishyouraccent/controller/RegisterController.java
+++ src/main/java/org/accentreductionapps/polishyouraccent/controller/RegisterController.java
@@ -1,27 +1,39 @@
 package org.accentreductionapps.polishyouraccent.controller;
 
+import org.accentreductionapps.polishyouraccent.model.users.User;
+import org.accentreductionapps.polishyouraccent.service.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.net.URI;
-
 @RestController
 public class RegisterController {
+
+    private final UserService userService;
+
+    public RegisterController(UserService userService) {
+        this.userService = userService;
+    }
     //GET - display page with form to add new user
     //POST - try to create user
 
     @GetMapping("/register")
     public ResponseEntity<String> registerSite() {
         //TODO RETURN PAGE
-        return ResponseEntity.created(URI.create("http://localhost:8080/register")).header("Register Page", "Some value?").body("Hello Register Page");
+        return ResponseEntity.ok("Welcome to Register Page!");
     }
 
-    @PostMapping("/register")
-    public boolean sendRequestNewAccountCreate(String emailAddress,String username, String password){
-
-        return false;
+    @PostMapping("/register/student")
+    public ResponseEntity<User> sendRequestNewStudentAccountCreate(String emailAddress, String password, Integer age) {
+        User newStudent = userService.createNewStudent(emailAddress, password, age);
+        return ResponseEntity.ok(newStudent);
     }
 
+    @PostMapping("/register/specialist")
+    public ResponseEntity<User> sendRequestNewSpecialistAccountCreate(String emailAddress, String password, Integer verificationCode) {
+        User newSpecialist = userService.createNewSpecialist(emailAddress, password, verificationCode);
+        return ResponseEntity.ok(newSpecialist);
+    }
 }
+
