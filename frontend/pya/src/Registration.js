9378f722dd3e2ca6297f5b9183962bd4c6d33b50
Marcin <marcin.szuwalski@motorolasolutions.com>
2023-02-01 14:33:57 +0100

    work in progress: registration form talks to REST API

--- frontend/pya/src/Registration.js
+++ frontend/pya/src/Registration.js
@@ -1,38 +1,88 @@
-import React, {Component} from 'react';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
+import React, { Component } from 'react';
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
 
-import './Registration.css';
+import './Login.css';
 
+import RegistrationAlert from './RegistrationAlert.js';
 
-class Registration extends Component {
+class Register extends Component {
+    constructor(props) {
+        super(props);
+        this.registrationAlert = React.createRef();
+    }
 
     handleSubmit = event => {
         event.preventDefault();
-        console.log(event.target.username.value);
-        console.log(event.target.password.value);
+        this.registerUser(event.target.name.value, event.target.password.value);
+    }
+
+    registerUser(name, password) {
+        fetch('http://localhost:8080/users', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                name: name,
+                password: password,
+                emailAddress: 'FoxMulder@fbi.gov',
+            })
+        }).then(function(response) {
+            if (response.status === 200) {
+                this.showRegistrationAlert("success", "Great success!", "You can now log in using your credentials.");
+            } else if (response.status === 422) {
+                this.showRegistrationAlert("danger", "User already exists", "Please choose a different name.");
+            } else {
+                this.showRegistrationAlert("danger", "User not registered!", "Something went wrong.");
+            }
+        }.bind(this)).catch(function(error) {
+            this.showRegistrationAlert("danger", "Error", "Something went wrong.");
+        }.bind(this));
+    }
+
+    showRegistrationAlert(variant, heading, message) {
+        this.registrationAlert.current.setVariant(variant);
+        this.registrationAlert.current.setHeading(heading);
+        this.registrationAlert.current.setMessage(message);
+        this.registrationAlert.current.setVisible(true);
     }
 
     render() {
-        return <div className="Register">
+        return (
+            <>
+                <div className = "Login" >
+                    <p style={{fontSize: `0.9rem`, color: `orange`}}>   -   I'm working here!</p>
+                    <h1 className="RegisterHeader">Register</h1>
 
-            <Form onSubmit={this.handleSubmit}>
-                <Form.Group controlId="username" size="lg">
-                    <Form.Label>
-                        Username
-                    </Form.Label>
-                    <Form.Control autoFocus name="username"/>
-                </Form.Group>
-                <Form.Group controlId="password" size="lg">
-                    <Form.Label>
-                        Password
-                    </Form.Label>
-                    <Form.Control type="password" name="password"/>
-                </Form.Group>
-                <Button type="submit">Register</Button>
-            </Form>
-        </div>
-    };
+                    <Form onSubmit = { this.handleSubmit } >
+
+                        <Form.Group controlId = "name" size = "lg">
+                            <Form.Label> Username </Form.Label>
+                            <Form.Control autoFocus name = "name"/>
+                        </Form.Group>
+
+                        <Form.Group controlId = "password" size = "lg" >
+                            <Form.Label > Password </Form.Label>
+                            <Form.Control type = "password" name = "password"/>
+                        </Form.Group>
+                        <br></br>
+
+                        <Button className={'RegistrationButton'} block size = "lg" type = "submit">
+                            Register
+                        </Button>
+
+                    </Form>
+
+                </div>
+
+                <RegistrationAlert ref = { this.registrationAlert }/>
+
+            </>
+        );
+    }
+
 }
 
-export default Registration;
+export default Register;
